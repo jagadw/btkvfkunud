46647248413936eac693a277fc848c38de1a20fe
jagadw <jagadhitawicaksana@gmail.com>
2025-06-13 17:30:10 +0800

    create a folder for each table

--- resources/views/layouts/partials/admin/footer.blade.php
+++ resources/views/layouts/partials/admin/footer.blade.php
@@ -2,13 +2,13 @@
     <!--begin::Footer container-->
     <div class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
         <!--begin::Copyright-->
-        <div class="text-dark order-2 order-md-1">
+        {{-- <div class="text-dark order-2 order-md-1">
             <span class="text-muted fw-semibold me-1">2025&copy;</span>
             <a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">Cakra Jaya</a>
-        </div>
+        </div> --}}
         <!--end::Copyright-->
         <!--begin::Menu-->
-        <ul class="menu menu-gray-600 menu-hover-primary fw-semibold order-1">
+        {{-- <ul class="menu menu-gray-600 menu-hover-primary fw-semibold order-1">
             <li class="menu-item">
                 <a href="https://keenthemes.com" target="_blank" class="menu-link px-2">About</a>
             </li>
@@ -18,7 +18,7 @@
             <li class="menu-item">
                 <a href="https://1.envato.market/EA4JP" target="_blank" class="menu-link px-2">Purchase</a>
             </li>
-        </ul>
+        </ul> --}}
         <!--end::Menu-->
     </div>
     <!--end::Footer container-->
